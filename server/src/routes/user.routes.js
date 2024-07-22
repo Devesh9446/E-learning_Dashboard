550686803c063e5545b8c51b9b9b4a0128dd3ec6
Devesh9446 <123630051+Devesh9446@users.noreply.github.com>
2024-07-22 23:05:32 +0530

    learner controller updated

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -4,5 +4,8 @@ import {add_learners} from '../controllers/learners.controllers.js'
 const router =Router()
 
 router.route('/learner').post(add_learners)
+router.route('/learner').get(_learners)
+router.route('/learner').patch(add_learners)
+router.route('/learner').delete(add_learners)
 
 export default router
